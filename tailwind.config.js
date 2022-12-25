24a519bd6755a2641a4f181ed72f871e157354a9
JasonIsAzn <lejason123@gmail.com>
2022-12-25 01:47:22 -0600

    update dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "var(--primary-color)",
+        "secondary-color": "var(--secondary-color)",
+        "bg0": "var(--bg0)",
+        "bg1": "var(--bg1)",
+        "bg2": "var(--bg2)",
+        "fg0": "var(--fg0)",
+        "green-color0": "var(--green-color0)",
+        "green-color1": "var(--green-color1)",
+        "red-color0": "var(--red-color0)",
+        "purple-color0": "var(--purple-color0)"
+      },
+    },
+  },
+  plugins: [],
+};

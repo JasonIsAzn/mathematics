8189d8a24dfed78732ffd0cb06e73e60c4445332
JasonIsAzn <lejason123@gmail.com>
2022-12-25 01:49:01 -0600

    add terminal route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Home from "./frontend/pages/Home";
+import Terminal from "./frontend/pages/Terminal";
 
 import React from "react";
 import { Routes, Route } from "react-router-dom";
@@ -8,6 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/terminal" element={<Terminal/>} />
     </Routes>
   );
 }

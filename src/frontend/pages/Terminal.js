aadc10a106389a2980393978a155edb528082c46
JasonIsAzn <lejason123@gmail.com>
2022-12-29 00:03:59 -0600

    format changes

--- src/frontend/pages/Terminal.js
+++ src/frontend/pages/Terminal.js
@@ -1,10 +1,9 @@
 import React from "react";
 
 export default function Terminal() {
-    return (
-        
-        <div>
-            <h1>Welcome to Terminal</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Welcome to Terminal</h1>
+    </div>
+  );
 }
